830e4a8970918ee71c0a81e05cf01cbda55d55a3
xrayfinding <17862701238@163.com>
2021-01-12 20:21:59 +0800

    just so so , and the RH leg have some problem

--- locomotion/robots/laikago.py
+++ locomotion/robots/laikago.py
@@ -23,6 +23,7 @@
 """Pybullet simulation of a Laikago robot."""
 import math
 import re
+import numba
 import numpy as np
 import pybullet as pyb  # pytype: disable=import-error
 
@@ -68,6 +69,10 @@ _DEFAULT_HIP_POSITIONS = (
     (-0.21, 0.1157, 0),
 )
 
+HIP_OFFSETS = np.array([[0.21, -0.1157, 0.], [0.21, 0.1157, 0.],
+                        [-0.21, -0.1157, 0.], [-0.21, 0.1157, 0.]
+                        ])
+
 ABDUCTION_P_GAIN = 220.0
 ABDUCTION_D_GAIN = 0.3
 HIP_P_GAIN = 220.0
@@ -87,6 +92,7 @@ _MOTOR_NAME_PATTERN = re.compile(r"\w+_hip_motor_\w+")
 _KNEE_NAME_PATTERN = re.compile(r"\w+_lower_leg_\w+")
 _TOE_NAME_PATTERN = re.compile(r"jtoe\d*")
 
+#/usr/local/lib/python3.7/dist-packages/pybullet_data文件夹下
 URDF_FILENAME = "laikago/laikago_toes_limits.urdf"
 
 _BODY_B_FIELD_NUMBER = 2
@@ -95,12 +101,92 @@ _LINK_A_FIELD_NUMBER = 3
 UPPER_BOUND = 6.28318548203
 LOWER_BOUND = -6.28318548203
 
+@numba.jit(nopython=True, cache=True)
+def foot_position_in_hip_frame_to_joint_angle(foot_position, l_hip_sign=1):
+  l_up = 0.25
+  l_low = 0.25
+  l_hip = 0.037 * l_hip_sign
+  x, y, z = foot_position[0], foot_position[1], foot_position[2]
+  theta_knee = -np.arccos(
+      (x**2 + y**2 + z**2 - l_hip**2 - l_low**2 - l_up**2) /
+      (2 * l_low * l_up))
+  l = np.sqrt(l_up**2 + l_low**2 + 2 * l_up * l_low * np.cos(theta_knee))
+  theta_hip = np.arcsin(-x / l) - theta_knee / 2
+  c1 = l_hip * y - l * np.cos(theta_hip + theta_knee / 2) * z
+  s1 = l * np.cos(theta_hip + theta_knee / 2) * y + l_hip * z
+  theta_ab = np.arctan2(s1, c1)
+  return np.array([theta_ab, theta_hip, theta_knee])
+
+
+@numba.jit(nopython=True, cache=True)
+def foot_position_in_hip_frame(angles, l_hip_sign=1):
+  theta_ab, theta_hip, theta_knee = angles[0], angles[1], angles[2]
+  l_up = 0.25
+  l_low = 0.25
+  l_hip = 0.037 * l_hip_sign
+  leg_distance = np.sqrt(l_up**2 + l_low**2 +
+                         2 * l_up * l_low * np.cos(theta_knee))
+  eff_swing = theta_hip + theta_knee / 2
+
+  off_x_hip = -leg_distance * np.sin(eff_swing)
+  off_z_hip = -leg_distance * np.cos(eff_swing)
+  off_y_hip = l_hip
+
+  off_x = off_x_hip
+  off_y = np.cos(theta_ab) * off_y_hip - np.sin(theta_ab) * off_z_hip
+  off_z = np.sin(theta_ab) * off_y_hip + np.cos(theta_ab) * off_z_hip
+  return np.array([off_x, off_y, off_z])
+
+
+@numba.jit(nopython=True, cache=True)
+def analytical_leg_jacobian(leg_angles, leg_id):
+  """
+  Computes the analytical Jacobian.
+  Args:
+  ` leg_angles: a list of 3 numbers for current abduction, hip and knee angle.
+    l_hip_sign: whether it's a left (1) or right(-1) leg.
+  """
+  l_up = 0.25
+  l_low = 0.25
+  l_hip = 0.037  * (-1)**(leg_id + 1)
+
+  t1, t2, t3 = leg_angles[0], leg_angles[1], leg_angles[2]
+  l_eff = np.sqrt(l_up**2 + l_low**2 + 2 * l_up * l_low * np.cos(t3))
+  t_eff = t2 + t3 / 2
+  J = np.zeros((3, 3))
+  J[0, 0] = 0
+  J[0, 1] = -l_eff * np.cos(t_eff)
+  J[0, 2] = l_low * l_up * np.sin(t3) * np.sin(t_eff) / l_eff - l_eff * np.cos(
+      t_eff) / 2
+  J[1, 0] = -l_hip * np.sin(t1) + l_eff * np.cos(t1) * np.cos(t_eff)
+  J[1, 1] = -l_eff * np.sin(t1) * np.sin(t_eff)
+  J[1, 2] = -l_low * l_up * np.sin(t1) * np.sin(t3) * np.cos(
+      t_eff) / l_eff - l_eff * np.sin(t1) * np.sin(t_eff) / 2
+  J[2, 0] = l_hip * np.cos(t1) + l_eff * np.sin(t1) * np.cos(t_eff)
+  J[2, 1] = l_eff * np.sin(t_eff) * np.cos(t1)
+  J[2, 2] = l_low * l_up * np.sin(t3) * np.cos(t1) * np.cos(
+      t_eff) / l_eff + l_eff * np.sin(t_eff) * np.cos(t1) / 2
+  return J
+
+@numba.jit(nopython=True, cache=True, parallel=True)
+def foot_positions_in_base_frame(foot_angles):
+  foot_angles = foot_angles.reshape((4, 3))
+  foot_positions = np.zeros((4, 3))
+  for i in range(4):
+    foot_positions[i] = foot_position_in_hip_frame(foot_angles[i],
+                                                   l_hip_sign=(-1)**(i + 1))
+  
+  return foot_positions + HIP_OFFSETS
+
 
 class Laikago(minitaur.Minitaur):
   """A simulation for the Laikago robot."""
   MPC_BODY_MASS = 215/9.8
-  MPC_BODY_INERTIA = (0.07335, 0, 0, 0, 0.25068, 0, 0, 0, 0.25447)
+  MPC_BODY_INERTIA = np.array((0.07335, 0, 0, 0, 0.25068, 0, 0, 0, 0.25447)) * 4.
+  # down the body
   MPC_BODY_HEIGHT = 0.42
+  #MPC_BODY_HEIGHT = 0.38
+  MPC_VELOCITY_MULTIPLIER = 0.5
   ACTION_CONFIG = [
       locomotion_gym_config.ScalarField(name="motor_angle_0",
                                         upper_bound=UPPER_BOUND,
@@ -241,11 +327,21 @@ class Laikago(minitaur.Minitaur):
 
     return contacts
 
+  def GetFootPositionsInBaseFrame(self):
+    """Get the robot's foot position in the base frame."""
+    motor_angles = self.GetMotorAngles()
+    return foot_positions_in_base_frame(motor_angles)
+
   def ComputeJacobian(self, leg_id):
     """Compute the Jacobian for a given leg."""
     # Because of the default rotation in the Laikago URDF, we need to reorder
     # the rows in the Jacobian matrix.
-    return super(Laikago, self).ComputeJacobian(leg_id)[(2, 0, 1), :]
+    
+    #golaoxu： error in this function, change it 
+    #return super(Laikago, self).ComputeJacobian(leg_id)[(2, 0, 1), :]
+
+    motor_angles = self.GetMotorAngles()[leg_id * 3:(leg_id + 1) * 3]
+    return analytical_leg_jacobian(motor_angles, leg_id)
 
   def ResetPose(self, add_constraint):
     del add_constraint
@@ -382,3 +478,40 @@ class Laikago(minitaur.Minitaur):
   def GetConstants(cls):
     del cls
     return laikago_constants
+""" 
+  def ComputeMotorAnglesFromFootLocalPosition(self, leg_id,
+                                              foot_local_position):
+      
+      use IK to compute the motor angles, given the foot link's local position.
+
+      Args:
+        leg_id: The leg index.
+        foot_local_postion: The foot link's position in the base frame.
+
+      Returns:
+        A tuple: The position indices and the angles for all joints alone the 
+        leg. The position indices is consistent with the joint orders as returned
+        by GetMotorAngles API. 
+      
+      assert len(self._foot_link_ids) == self.num_legs
+      # toe_id = self._foot_link_ids[leg_id]
+      # 整数除法
+      motors_per_leg = self.num_motors // self.num_legs
+      joint_position_idxs = list(
+          range(leg_id * motors_per_leg,
+                leg_id * motors_per_leg + motors_per_leg))
+
+      joint_angles = foot_position_in_hip_frame_to_joint_angle(
+          foot_local_position - HIP_OFFSETS[leg_id],
+          l_hip_sign=(-1)**(leg_id + 1))
+
+      # Joint offset is necessary for Laikago.
+      joint_angles = np.multiply(
+          np.asarray(joint_angles) -
+          np.asarray(self._motor_offset)[joint_position_idxs],
+          self._motor_direction[joint_position_idxs])
+
+      # Return the joing index (the same as when calling GetMotorAngles) as well
+      # as the angles.
+      return joint_position_idxs, joint_angles.tolist()
+  """ 
